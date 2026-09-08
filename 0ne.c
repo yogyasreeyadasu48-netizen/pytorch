f2ec273095b705e7b7ba440ac001ef78d0dcc420
yogyasreeyadasu48-netizen <yogyasreeyadasu48@gmail.com>
2026-09-08 10:23:42 +0530

    Add files via upload

---
+++ 0ne.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void int()
+{
+	int a;
+	printf("enter a number:");
+	scanf("%d",&a);
+	if(a>o)
+	printf("%d  is positive num\n");
+		else if(a<0)
+			printf("%d is negative num\n");
+		else
+			printf("%d is zero");
+	return 0;
+}            
